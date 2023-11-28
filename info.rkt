30a547c7babf1b71c13fc3ebef21ed0e0ba81517
Raffi Sanna <rvs314@protonmail.com>
2023-11-28 11:39:44 -0500

    Initial repo

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "Qiease")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/Qiease.scrbl" ())))
+(define pkg-desc "Qi with Ease")
+(define version "0.0")
+(define pkg-authors '(rvs314))
+(define license 'MIT)
---
+++ scribblings/Qiease.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[Qiease
+                    racket/base]]
+
+@title{Qiease}
+@author{rvs314}
+
+@defmodule[Qiease]
+
+Package Description Here
