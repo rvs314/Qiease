1467db83f5217810ef4a11bb375bc4d0f10c1eac
Raffi Sanna <rvs314@protonmail.com>
2023-11-28 11:52:24 -0500

    Remove license

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
 (define collection "Qiease")
-(define deps '("base"))
+(define deps '("base" "qi"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/Qiease.scrbl" ())))
 (define pkg-desc "Qi with Ease")
 (define version "0.0")
 (define pkg-authors '(rvs314))
-(define license 'MIT)
